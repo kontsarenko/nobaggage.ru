100b37acbf0fb7760e9b37093844d65ca5528269
kontsarenko <kontsarenko@gmail.com>
2018-01-23 15:30:40 +0300

    Delete connect.php

--- cfg/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = mysql_connect("localhost", "nobaggag_admin", "LbJR8UAe") or die ("Ошибка подключения к базе данных");
-mysql_query("SET NAMES utf8");
-mysql_select_db("nobaggag_core", $db);
-mysql_query("SET lc_time_names = 'ru_RU'");
-?>
